f286b34e1354a191ad5493811e01e634c0eff48c
sanjay <s.sanjaykrishna41@gmail.com>
2021-02-25 17:46:34 +0530

    datatypes and variables

---
+++ src/1.Datatype_variables.kt
@@ -0,0 +1,11 @@
+fun main(args : Array<String>) : Unit{
+    //variable declared with var are mutable can be changed
+    var string : String = "Hello World"
+    //variable declared with var are mutable can be changed
+    val str = "Kotlin Program"
+
+//    string = 10 //compilation error
+
+    println(string);
+    println(str)
+}
