72b59a901bd2ae0eb25dae3d190fcfce5ca2924a
sanjay <s.sanjaykrishna41@gmail.com>
2021-02-26 15:11:33 +0530

    Constructors

--- src/2.Function_Class.kt
+++ src/2.Function_Class.kt
@@ -1,8 +1,7 @@
 fun main(args : Array<String>){
-    var name = "Sanjay";
-
+    var name="Sanjay Krishna"
     //creating an object of class person and passing name as parameter to display function
-    var person = Person(name,20)
+    val person = Person(name,20)
     person.display()
     //string interpolation
     println("age of $name is ${person.age}")
