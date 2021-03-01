c5b4d7930efc7184775e985361fae79efe68ac0d
sanjay <s.sanjaykrishna41@gmail.com>
2021-03-01 13:12:41 +0530

    function and functions as expressions

---
+++ src/11.functions_demo.kt
@@ -0,0 +1,13 @@
+//main function
+fun main(arg:Array<String>){
+    println(addNumbers(1,2))
+    val res = max(3,4)
+    println("$res is greater")
+}
+//function to add two numbers
+fun addNumbers(a:Int,b:Int) : String{
+    return "Sum of $a + $b = ${a+b}"
+}
+
+//one-line function - function as expression
+fun max(a:Int, b: Int) : Int = if(a>b) {a} else {b}
