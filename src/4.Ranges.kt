30f7d69638ff3b9a77a54977f44976766cef7a50
sanjay <s.sanjaykrishna41@gmail.com>
2021-03-01 10:54:01 +0530

    RANGES and DOUBLE DOT

---
+++ src/4.Ranges.kt
@@ -0,0 +1,22 @@
+fun main(args:Array<String>){
+    var r1 = 1..2   //This range contains the number 1,2,3,4,5
+    println(r1)
+
+    var r2 = 5 downTo 1  //This range contains the number 5,4,3,2,1
+    println(r2)
+
+    var r3 = 5 downTo 1 step 2 //prints numbers 5,3,1
+    println(r3)
+
+    var r4 = 'a' .. 'z'
+    println(r4)
+
+    var isPresent = 'c' in r4
+    println(isPresent)
+
+    var countDown = 10.downTo(1)
+    println(countDown)
+
+    var moveUp = 1.rangeTo(10)
+    println(moveUp)
+}
