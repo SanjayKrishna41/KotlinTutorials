7fded4f5cf35e6b41e7f8a8b3f01abfe6b9dc32b
sanjay <s.sanjaykrishna41@gmail.com>
2021-03-02 12:54:56 +0530

    Class Definition, init Block and Primary Constructor

--- src/oops_concept/1.class_program.kt
+++ src/oops_concept/1.class_program.kt
@@ -2,13 +2,13 @@ package oops_concept
 
 fun main(args : Array<String>){
 
-    val student = Student("sanjay")
+    Student("sanjay")
 }
 
 //class with primary constructor
 class Student (var name:String){
     //init block (get executed after instance(after object creation) of class is created)
     init {
-        println("My name is $name")
+        println("My name is ${this.name}")
     }
 }
