aab92e69019630ba9da3da561052b47b5bb3f5e8
sanjay <s.sanjaykrishna41@gmail.com>
2021-03-02 13:33:35 +0530

    overriding

--- src/oops_concept/2.inheritance.kt
+++ src/oops_concept/2.inheritance.kt
@@ -5,35 +5,49 @@ fun main(args: Array<String>) {
     val cat = Cat()
     dog.eat()
     cat.sleep()
+    println(dog.className)
+    println(cat.className)
 }
 
-open class Animal(var className: String) {
-    init {
-        this.className = className
-    }
+open class Animal() {
 
-    fun eat() {
-        println("${className} eat")
-    }
+    open var className: String=""
 
-    fun sleep() {
-        println("${className} sleep")
+    //using open keyword, this function can be overridden
+    open fun eat() {
+        println("Animal eating")
+    }
+    open fun sleep() {
+        println("Animal sleep")
     }
 }
 
-class Dog : Animal("Dog"){
-
+class Dog : Animal() {
     var breed: String = ""
-
+    override var className:String = "Dog"
     fun bark() {
         println("bark")
     }
+    //function overriding
+    override fun eat() {
+        //calls the parent eat() method
+        super<Animal>.eat()
+        println("Dog is eating")
+    }
 }
 
-class Cat : Animal("Cat") {
+class Cat : Animal() {
+    override var className:String = "Cat"
     var age: Int = -1
-
     fun mew() {
         println("mew")
+    }
+    //function overriding
+    override fun eat() {
+        println("Cat is eating")
+    }
+    //function overriding
+    override fun sleep() {
+        println("Cat is Sleeping")
     }
 }
