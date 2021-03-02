5ea15b40b2050a6c706a6778ce0e3b2e1fc5d876
sanjay <s.sanjaykrishna41@gmail.com>
2021-03-02 13:04:19 +0530

    secondary constructor

--- src/oops_concept/1.class_program.kt
+++ src/oops_concept/1.class_program.kt
@@ -3,12 +3,22 @@ package oops_concept
 fun main(args : Array<String>){
 
     Student("sanjay")
+    //call to secondary constructor
+    Student("krishna",10)
 }
 
 //class with primary constructor
-class Student (var name:String){
+class Student (private var name:String){
+    var age = 0
+
     //init block (get executed after instance(after object creation) of class is created)
     init {
-        println("My name is ${this.name}")
+        println("Primary Constructor    : My name is ${this.name}")
+    }
+    //secondary constructor
+    constructor(name:String,age:Int):this(name){
+        //the body of secondary constructor is called after the init block
+        this.age = age
+        println("Secondary Constructor  : Age of $name is $age")
     }
 }
